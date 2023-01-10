28f477a73d7f74eb32d260867f3b3e84b6d6ab12
gus <agustin.sanchez3150@gmail.com>
2023-01-10 09:46:57 -0600

    Apariencia cambiada

--- tkinterExample.py
+++ tkinterExample.py
@@ -9,25 +9,28 @@ def calculate(*args):
         pass
 
 root = Tk()
-root.title("Feet to Meters")
+root.title("Copy Files")
 
+#Apariencia
 mainframe = ttk.Frame(root, padding="3 3 12 12")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+#root.geometry("600x400")
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
 feet = StringVar()
-feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
-feet_entry.grid(column=2, row=1, sticky=(W, E))
+feet_entry = ttk.Entry(mainframe, width=30, textvariable=feet)
+feet_entry.grid(column=3, row=1, sticky=(W, E))
 
 meters = StringVar()
-ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+ttk.Label(mainframe, textvariable=meters).grid(column=3, row=2, sticky=(W, E))
 
-ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
+ttk.Button(mainframe, text="Inciar", command=calculate, width=30).grid(column=3, row=3, sticky=W)
 
-ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
-ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
-ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+#Labels
+ttk.Label(mainframe, text="Ruta Inicial", width=15).grid(column=1, row=1, sticky=W)
+#ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
+ttk.Label(mainframe, text="Status", width=15).grid(column=1, row=2, sticky=W)
 
 for child in mainframe.winfo_children(): 
     child.grid_configure(padx=5, pady=5)
