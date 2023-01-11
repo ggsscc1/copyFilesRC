7d3d0c4ed4dc58517b6cdfeffbf2af06b318ab64
gus <agustin.sanchez3150@gmail.com>
2023-01-11 15:20:22 -0600

    Funcionando copea archivos origen-destino

--- prueba1.py
+++ prueba1.py
@@ -7,24 +7,25 @@ from tkinter import ttk
 def changeStatus(status):
   ttk.Label(mainframe, text=status).grid(column=3, row=2, sticky=E)
 
-# Create a function that increases the counter by 1 when the button is clicked
+# Create a function that copy files depending of the path
 def start_copy():
     changeStatus("                                            ")
     changeStatus("Copeando archivos")
     
-    # Establece la ruta de la carpeta de origen y la ruta de la carpeta de destino
+    #Establece la ruta de la carpeta de origen y la ruta de la carpeta de destino
     #origen = r'C:\Users\Administrador\Desktop\PRUEBAS_PYTON\origen'
     #destino = r'C:\Users\24\Desktop\copy files RC python\destino'
-    #origen = r'E:\2008\TAMUIN01_2008\2008\TAMUIN\1\Exportar\Pdf'
+    origen = r'C:\Users\24\Desktop\copy files RC python\origen'
     #origen = '\dev\cdrom'
-    #destino = r'C:\Users\Administrador\Desktop\PRUEBAS_PYTON\destino'
+    destino = r'C:\Users\24\Desktop\copy files RC python\destino'
+    #destino = r(ruta)
     # Obtiene la lista de archivos en la carpeta de origen
-    #archivos = os.listdir(origen)
+    archivos = os.listdir(origen)
 
     # Recorre la lista de archivos y los copia a la carpeta de destino
-    #for archivo in archivos:
-      #ruta_archivo = os.path.join(origen, archivo)
-      #shutil.copy(ruta_archivo, destino)
+    for archivo in archivos:
+      ruta_archivo = os.path.join(origen, archivo)
+      shutil.copy(ruta_archivo, destino)
     changeStatus("                                             ")
     changeStatus("Finalizado")
     print(ruta.get())
@@ -34,29 +35,28 @@ def start_copy():
 # Create the main window
 root = Tk()
 root.title("Copy Files")
-#root.geometry("600x400")
 
 #Apariencia
 mainframe = ttk.Frame(root, padding="3 3 12 12")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-  #root.geometry("600x400")
+#root.geometry("600x400")
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
-
+#Variable ruta that is used on the function
 ruta = StringVar()
+# entry textBox 
 ruta_entry = ttk.Entry(mainframe, width=30, textvariable=ruta)
 ruta_entry.grid(column=3, row=1, sticky=(W, E))
 
-ttk.Button(mainframe, text="Inciar", command=start_copy, width=30).grid(column=3, row=3, sticky=W)
-
 #Labels
 ttk.Label(mainframe, text="Ruta Inicial", width=15).grid(column=1, row=1, sticky=W)
-
+ttk.Label(mainframe, text="Status", width=15).grid(column=1, row=2, sticky=W)
+#Button that calls the main function
+ttk.Button(mainframe, text="Inciar", command=start_copy, width=30).grid(column=3, row=3, sticky=W)
 changeStatus("Introduzca ruta inicial")
 
-ttk.Label(mainframe, text="Status", width=15).grid(column=1, row=2, sticky=W)
-
+# size of principal rectangle
 for child in mainframe.winfo_children(): 
     child.grid_configure(padx=5, pady=5)
 
